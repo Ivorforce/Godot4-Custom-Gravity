6cd09b026432f76a145149a806732331aa9813ae
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2022-08-22 22:50:55 +0200

    Stabilize and simplify screw gravities by using tranforms.

--- forces/ScrewGravity.gd
+++ forces/ScrewGravity.gd
@@ -1,30 +1,16 @@
 class_name ScrewGravity extends Spatial
 
-export var full_rotation_vector = Vector3(1, 0, 0)
-export var start_down = Vector3.DOWN
-export var is_clockwise = true
-export var acceleration = 9.81
-
-var _rotation_vector: Vector3
-var _distance_per_rotation: float
-
-var _a: Vector3
-var _b: Vector3
-
-func _ready():
-	reconfigure_from_params();
-
-func reconfigure_from_params():
-	_a = start_down
-	_b = full_rotation_vector.normalized().cross(_a)
-	
-	_distance_per_rotation = full_rotation_vector.length()
-	_rotation_vector = full_rotation_vector / _distance_per_rotation
-	
-	if not is_clockwise:
-		_b = -_b
+export var full_rotation_length := 1
+export var is_clockwise := true
+export var acceleration := 9.81
 
 func get_acceleration_at(position: Vector3) -> Vector3:
-	var difference := position - global_transform.origin
-	var progression := difference.project(_rotation_vector).length() / _distance_per_rotation * 2 * PI
-	return (_a * cos(progression) + _b * sin(progression)) * acceleration
+	var p := global_transform.affine_inverse() * position
+	var progression := p.z / full_rotation_length * 2 * PI
+	
+	if not is_clockwise:
+		progression = -progression
+	
+	var gravity := Vector3.DOWN * cos(progression) + Vector3.RIGHT * sin(progression)
+
+	return (global_transform.basis.get_rotation_quat() * gravity) * acceleration
