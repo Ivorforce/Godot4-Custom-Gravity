012c508dbd7618b78e57f5f34ad9b4c9472e94fd
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2023-05-28 01:04:59 +0200

    Clean up Player.gd a bit.

--- player/Player.gd
+++ player/Player.gd
@@ -9,8 +9,6 @@ extends CharacterBody3D
 @export var torque_control_floor := 10.0
 @export var torque_control_air := 1.0
 
-var _velocity := Vector3.ZERO
-
 @onready var _balance_point: BalancePoint = $BalancePoint
 @onready var _camera_anchor: CameraAnchor = $CameraAnchor
 
@@ -27,82 +25,85 @@ static func get_movement_intention(basis: Basis, up: Vector3, vertical: float, h
 	return up_surface * intention_2d.y + right_surface * intention_2d.x
 
 func _physics_process(delta: float) -> void:
-	var acceleration := _balance_point.acceleration
-	var down := _balance_point.down
-	var up = _balance_point.up
-	
+	# Update where we are
 	_camera_anchor.target_origin = _balance_point.global_transform.origin
-	
+		
+	var acceleration := _balance_point.acceleration
 	if acceleration == Vector3.ZERO:
 		# Weeee floating in free space!
-		# Move tick
-		set_velocity(_velocity)
-		set_up_direction(Vector3.UP)
-		set_floor_stop_on_slope_enabled(true)
 		move_and_slide()
-		_velocity = velocity
 		return
 	
+	# Because we have it, update where up / down is
+	_camera_anchor.target_down = _balance_point.down
+	set_up_direction(_balance_point.up)
+
 	var movement_intention := get_movement_intention(
 		get_viewport().get_camera_3d().global_transform.basis,
-		up,
+		_balance_point.up,
 		Input.get_action_strength("back") - Input.get_action_strength("forward"),
 		Input.get_action_strength("right") - Input.get_action_strength("left")
 	)
 
-	var is_on_floor := self.is_on_floor()
-
-	var is_jumping : = is_on_floor and Input.is_action_just_pressed( "jump")
-	var snap_vector = down
-	
-	if is_jumping:
-		_velocity +=  up * jump_strength - _velocity.project(up)
-		snap_vector = Vector3.ZERO
-		
-	var desired_velocity_change := movement_intention * speed - _velocity
-	# Delete "up" component; we have no control over this
-	desired_velocity_change -= desired_velocity_change.project(up)
-	
 	var current_velocity_control: float
 	var current_torque_control: float
-	if is_on_floor:
+	if self.is_on_floor():
 		current_velocity_control = velocity_control_floor
 		current_torque_control = torque_control_floor
 	else:
 		current_velocity_control = velocity_control_air
 		current_torque_control = torque_control_air
+		
+	# Jumping
+	self._process_jumping()
 	
 	# Walking
-	_velocity = _velocity.move_toward(
-		_velocity + desired_velocity_change,
-		 current_velocity_control * delta
-	)
+	_process_walking(movement_intention, current_velocity_control, delta)
 	
-	# Gravity
-	_velocity += acceleration * delta
+	# Forces acting on us
+	velocity += acceleration * delta
 	
-	# Move tick
-	set_velocity(_velocity)
-	# TODOConverter40 looks that snap in Godot 4.0 is float, not vector like in Godot 3 - previous value `snap_vector`
-	set_up_direction(up)
-	set_floor_stop_on_slope_enabled(true)
+	# Finally, we move!
 	move_and_slide()
-	_velocity = velocity
 	
-	var look_intention_horizontal
+	# ... and turn
+	_process_turning(movement_intention, current_torque_control, delta)
+
+
+func _process_jumping():
+	var up := _balance_point.up
+	
+	var is_jumping := self.is_on_floor() and Input.is_action_just_pressed( "jump")
+	if is_jumping:
+		velocity += up * jump_strength - velocity.project(up)
+
+
+func _process_walking(movement_intention: Vector3, velocity_control: float, delta: float):
+	var up := _balance_point.up
+	
+	var desired_velocity_change := movement_intention * speed - velocity
+	# Delete "up" component; we have no control over this
+	desired_velocity_change -= desired_velocity_change.project(up)
+	
+	# Walking
+	velocity = velocity.move_toward(
+		velocity + desired_velocity_change,
+		 velocity_control * delta
+	)
+
+
+func _process_turning(movement_intention: Vector3, torque_control: float, delta: float):
+	var forward := -transform.basis.z
+	var up := _balance_point.up
+	
+	var look_intention_horizontal: Vector3
 	if movement_intention != Vector3.ZERO:
 		look_intention_horizontal = movement_intention
 	else:
-		look_intention_horizontal = -transform.basis.z
-		# Delete "up" component; we want to look straight at the horizon
-		look_intention_horizontal -= look_intention_horizontal.project(up)
+		look_intention_horizontal = forward - forward.project(up)
 	
-	# Basis.looking_at is currently not exposed :(
-	var look_intention := Transform3D.IDENTITY.looking_at(look_intention_horizontal, up).basis
+	var look_intention := Basis.IDENTITY.looking_at(look_intention_horizontal, up)
 	transform = Transform3D(
-		transform.basis.slerp(look_intention, current_torque_control * delta).orthonormalized(),
+		transform.basis.slerp(look_intention, torque_control * delta).orthonormalized(),
 		transform.origin
 	)
-	
-	_camera_anchor.target_down = down
-
